7fba9255ef02f4e98d54d8c59f8d34fdf3ad3384
Tsuyoshi Uchida <bangalorejapan@gmail.com>
2025-06-01 17:08:36 +0900

    chore: テストコードのままリリースしてしまったので本番コードに戻す

--- server/api/recommend.post.ts
+++ server/api/recommend.post.ts
@@ -3,7 +3,6 @@ import type { RecommendRequest, RecommendResponse } from '~/types/recommend';
 import { buildPrompt } from '~/server/util';
 import { randomUUID } from 'crypto';
 import OpenAI from 'openai';
-import { recommend } from '../data/recommend';
 
 const openai = new OpenAI({
   apiKey: useRuntimeConfig().openai.apiKey,
@@ -18,36 +17,35 @@ export default defineEventHandler(async (event): Promise<RecommendResponse> => {
   const body = await readValidatedBody<RecommendRequest>(event, recommendRequestSchema.parse);
   const recommendationId = randomUUID();
 
-  // const prompt = buildPrompt(body);
+  const prompt = buildPrompt(body);
 
-  // const chat = await openai.chat.completions.create({
-  //   model: 'gpt-4',
-  //   messages: [{ role: 'user', content: prompt }],
-  //   temperature: 0.8,
-  // });
+  const chat = await openai.chat.completions.create({
+    model: 'gpt-4',
+    messages: [{ role: 'user', content: prompt }],
+    temperature: 0.8,
+  });
 
-  // const content = chat.choices[0]?.message?.content?.trim();
-  // if (!content) {
-  //   throw createError({
-  //     statusCode: 500,
-  //     message: 'OpenAIの応答が空でした',
-  //   });
-  // }
+  const content = chat.choices[0]?.message?.content?.trim();
+  if (!content) {
+    throw createError({
+      statusCode: 500,
+      message: 'OpenAIの応答が空でした',
+    });
+  }
 
-  // let json: unknown;
-  // try {
-  //   json = JSON.parse(content);
-  // } catch (err) {
-  //   throw createError({
-  //     statusCode: 500,
-  //     message: 'OpenAIからのJSON解析に失敗しました',
-  //   });
-  // }
-  // const result = recommendResponseSchema.parse(json);
-  return recommend;
+  let json: unknown;
+  try {
+    json = JSON.parse(content);
+  } catch (err) {
+    throw createError({
+      statusCode: 500,
+      message: 'OpenAIからのJSON解析に失敗しました',
+    });
+  }
+  const result = recommendResponseSchema.parse(json);
 
-  // return {
-  //   ...result,
-  //   id: recommendationId,
-  // };
+  return {
+    ...result,
+    id: recommendationId,
+  };
 });
